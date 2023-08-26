a31be6f0a7d3f623b6aff40acf60b375b3e31003
fessycode <fessydoris@gmail.com>
2023-08-26 11:48:12 -0700

    update

--- sphere_main.py
+++ sphere_main.py
@@ -2,7 +2,7 @@
 
 def main():
     try:
-        radius = float(input("Enter the value of radius: "))
+        radius = float(input("Enter the value of radius "))
         if radius < 0:
             print(" The value of radius can not be negative")
         else:
